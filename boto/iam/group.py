a6bbaf5d4df733e0abc50875681614c9f428970b
Gertjan Oude Lohuis <gertjan@byte.nl>
2012-08-29 16:18:36 +0200

    Wrapper objects for Group, User, Role, InstanceProfile and ServerCertificate

---
+++ boto/iam/group.py
@@ -0,0 +1,67 @@
+# Copyright (c) 2012 Gertjan Oude Lohuis, Byte Internet http://byte.nl/
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+from boto.resultset import ResultSet
+from boto.iam.user import User
+
+
+class Group(object):
+    """
+    Represents an IAM Group
+
+    :ivar arn: The Amazon Resource Name (ARN) specifying the group
+    :ivar create_date: The date when the group was created
+    :ivar id: The stable and unique string identifying the group
+    :ivar name: The name identifying the group
+    :ivar path: Path to the group
+    """
+
+    def __init__(self, connection=None, name=None, endpoints=None):
+        self.connection  = connection
+        self.name        = name
+        self.arn         = None
+        self.id          = None
+        self.path        = None
+        self.users       = None
+
+    def __repr__(self):
+        return 'Group:%s' % self.name
+
+    def startElement(self, name, attrs, connection):
+        if name == 'Users':
+            self.users = ResultSet([('member', User)])
+            return self.users
+        else:
+            return None
+
+    def endElement(self, name, value, connection):
+        if name == 'Arn':
+            self.arn = value
+        elif name == 'Path':
+            self.path = value
+        elif name == 'GroupId':
+            self.id = value
+        elif name == 'GroupName':
+            self.name = value
+        else:
+            setattr(self, name, value)
+
+
--- boto/iam/servercertificate.py
+++ boto/iam/servercertificate.py
@@ -14,7 +14,7 @@
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
 # OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
 # ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
-# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 # WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
@@ -23,8 +23,16 @@
 class ServerCertificate(object):
     """
     Represents an IAM ServerCertificate
+
+    :ivar arn: The Amazon Resource Name (ARN) specifying the server certificate
+    :ivar body: The contents of the public key certificate
+    :ivar chain: The contents of the public key certificate chain
+    :ivar id: The stable and unique string identifying the server certificate
+    :ivar name: The name identifying the server certificate
+    :ivar path: Path to the server certificate
+    :ivar upload_date: The date when the server certificate was uploaded
     """
-    
+
     # Note: ServerCertificateName is used as unique identifier
 
     def __init__(self, connection=None, name=None):
@@ -36,13 +44,13 @@ class ServerCertificate(object):
         self.id          = None
         self.path        = None
         self.upload_date = None
-    
+
     def __repr__(self):
         return 'ServerCertificate:%s' % self.name
-    
+
     def startElement(self, name, attrs, connection):
         pass
-    
+
     def endElement(self, name, value, connection):
         if name == 'Arn':
             self.arn = value
@@ -60,16 +68,16 @@ class ServerCertificate(object):
             self.upload_date = value
         else:
             setattr(self, name, value)
-    
+
     def modify(self, new_certname=None, new_path=None):
         """
         Updates the name and/or the path of the specified server certificate.
-        
+
         :type new_cert_name: string
         :param new_cert_name: The new name for the server certificate.
             Include this only if you are updating the
             server certificate's name.
-        
+
         :type new_path: string
         :param new_path: If provided, the path of the certificate will be
             changed to this path.
@@ -84,3 +92,4 @@ class ServerCertificate(object):
                 self.path = new_path
 
         return rs
+
